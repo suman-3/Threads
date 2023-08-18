565e0a88ae9ca401dcf1171e586955ae42027279
Suman Mondal <mondalsuman97322@gmail.com>
2023-08-18 14:06:59 +0530

    Added NewFeature

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -42,7 +42,7 @@ const LeftSidebar = () => {
         })}
       </div>
 
-      <div className='mt-10 px-6'>
+      <div className='mt-5 px-6'>
         <SignedIn>
           <SignOutButton signOutCallback={() => router.push("/sign-in")}>
             <div className='flex cursor-pointer gap-4 p-4'>
